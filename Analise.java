cc4ef740bc197498d4c4b538980589e983cd0c48
Valentim Prado <pradovalentim2@gmail.com>
2024-05-25 21:58:54 +0100

    first commit

---
+++ Analise.java
@@ -0,0 +1,94 @@
+/*------------------------------------
+Tema: Gestão de uma Padaria
+Nome: Valentim Loth Simão Prado
+Numero: 33031
+Ficheiro: Analise.java
+Data: 22.05.2024
+--------------------------------------*
+
+/*
+1. Objectivo
+Este projeto tem o objetivo gerenciar as
+
+2. Visao [Interfaces Graficas]
+- ApresentacaoVisao
+- LoginVisao
+- MenuPrincipal
+
+- EntradaVisao
+- EstoqueVisao
+- ProducaoVisao
+- VendaVisao
+
+3. Entidades Fortes e Seus Atributos (Modelo)
+
+- EntradaModelo
+	int id
+	String materia
+    String unidadeMedida
+    int qtdEntrada
+    String dataEntrada
+    String dataValidade
+    double custoUnit
+    double custoTotal
+
+- EstoqueModelo
+    int id
+    String tipo
+    EntradaModelo materiaP
+    ProducaoModelo produtoAcabado
+    int nivelMinimo
+    int nivelAtual
+    double precoTotal
+
+- ProducaoModelo
+	int id
+    EstoqueModelo materia
+    String tipo
+    String produto
+	int qtdProd
+    String dataProd
+    double custoProd
+
+- VendaModelo
+	int id
+	ProducaoModelo produto
+    int qtd
+    double precoUnit
+    String cliente
+    String dataVenda
+	
+4. Ficheiro
+- EntradaFile.dat
+- EstoqueFile.dat
+- ProducaoFile.dat
+- VendaFile.dat
+
+5. Tabelas de Apoio (Auxiliares) = Entidades Fracas
+- ProdutoAcabado.tab
+- MateriaPrima.tab
+- UnidadeMedida.tab
+
+
+6. Diversos
+6.1 - Implementação: Java Swing
+6.2 - IDE: VScode
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ MenuPrincipal.java
@@ -0,0 +1,65 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+
+public class MenuPrincipal extends JFrame implements ActionListener
+{
+    private JMenuBar menuBar;
+    private JMenu ficheiroMenu;
+    private JMenuItem novaMatPItem, editarMatPItem, eliminarMatPItem, sairMatPItem;
+
+
+    public MenuPrincipal()
+    {
+        super("Menu Principal");
+        instanciarObj();
+        setJMenuBar(menuBar);
+        setSize(800, 600);
+        setVisible(true);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+       
+
+    }
+
+    public void instanciarObj()
+    {
+        menuBar = new JMenuBar();
+        ficheiroMenu = new JMenu("Ficheiro");
+        novaMatPItem = new JMenuItem("Nova Materia Prima");
+        editarMatPItem = new JMenuItem("Editar Materia Prima");
+        eliminarMatPItem = new JMenuItem("Eliminar Materia Prima");
+        sairMatPItem = new JMenuItem("Sair");
+
+        ficheiroMenu.add(novaMatPItem);
+        ficheiroMenu.add(editarMatPItem);
+        ficheiroMenu.add(eliminarMatPItem);
+        ficheiroMenu.addSeparator();
+        ficheiroMenu.add(sairMatPItem);
+
+        menuBar.add(ficheiroMenu);
+
+        sairMatPItem.addActionListener(this);
+    }
+
+
+    public void actionPerformed(ActionEvent evt)
+	{
+		if(evt.getSource() == sairMatPItem)
+        {
+           int resultado = JOptionPane.showConfirmDialog(null,"Tem certeza que deseja sair?","Saindo",JOptionPane.YES_NO_OPTION);
+           if(resultado == 0)
+           {
+                dispose();
+           }
+        }
+	}
+
+
+    public static void main(String args[])
+    {
+        new MenuPrincipal();
+    }
+
+}
