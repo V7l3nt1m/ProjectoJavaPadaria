475fea30114f362378705fca7f2c9ee15eaf2496
Valentim Prado <pradovalentim2@gmail.com>
2024-06-07 22:41:50 +0100

    Apresentacao visao feita

--- ApresentacaoVisao.java
+++ ApresentacaoVisao.java
@@ -10,6 +10,7 @@ import java.awt.*;
 import java.awt.event.*;
 import SwingComponents.*;
 import Calendario.*;
+import javax.swing.border.EmptyBorder;
 import javax.swing.UIManager.*;
 
 
@@ -17,21 +18,97 @@ public class ApresentacaoVisao extends JFrame
 {
     private PainelCentro painelCentro;
     private PainelSul painelSul;
+    private PainelNorte painelNorte;
+
 
     public ApresentacaoVisao()
     {
         super("Tela de Boas Vindas");
-
+        definirTema();
+        getContentPane().add(painelNorte = new PainelNorte(), BorderLayout.NORTH);
         getContentPane().add(painelCentro = new PainelCentro(), BorderLayout.CENTER);
         getContentPane().add(painelSul = new PainelSul(), BorderLayout.SOUTH);
 
         setSize(800,600);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
         setVisible(true);
     }
 
-     class PainelSul extends JPanel implements ActionListener
+    class PainelNorte extends JPanel
+    {
+        private JLabel image, titulo, labelVazia;
+        private Color customColor;
+        
+        public PainelNorte()
+        {   
+            setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+            customColor = Color.decode("#e6e8e9");
+
+            setBackground(customColor);
+            add(image = new JLabel());
+            image.setIcon(new ImageIcon("imagens/logoIco.png"));
+            image.setAlignmentX(Component.CENTER_ALIGNMENT); // Centraliza o ícone horizontalmente
+
+            add(labelVazia = new JLabel(" "));
+            labelVazia.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+            titulo = new JLabel("Termos e Condições");
+            titulo.setAlignmentX(Component.CENTER_ALIGNMENT);
+            titulo.setFont(new Font("Monospaced", Font.BOLD,24)); // Centraliza o texto horizontalmente
+            add(titulo);
+
+            add(labelVazia = new JLabel(" "));
+            labelVazia.setAlignmentX(Component.CENTER_ALIGNMENT);
+        }
+    }
+
+     
+
+    class PainelCentro extends JPanel implements ActionListener
+    {
+        JTextArea areaPrincipal;
+        JCheckBox concordarJCB;
+        JScrollPane scroll;
+
+        public PainelCentro()
+        {
+            setLayout(new GridLayout(2,1));
+
+
+            add(scroll = new JScrollPane(areaPrincipal = new JTextArea(21,20)));
+            areaPrincipal.setText("\n\nBem vindo ao Sistema de Gestao de Padaria (SGP) \n\n" + "Este projeto tem o objetivo gerenciar as entradas, saidas, processo de producao \ne estoque de uma padaria\n\n" + 
+            "Este projecto foi criado no ambito da cadeira de Fundamentos de Programação II no\n" + 
+            "Curso de Engenharia Informática da UCAN. \n" + 
+            "\nDados do Programador:\n" + "Nome: Valentim Loth Simão Prado\n" + "Nº Processo: 1000033031\n" + 
+            "\nEste sistema é de uso exclusivo aos estudantes e professores desta instituição." + 
+            "");
+            
+            areaPrincipal.setFont(new Font("Monospaced", Font.PLAIN,15));
+            areaPrincipal.setOpaque(false); // Torna o JTextArea transparente
+            areaPrincipal.setBorder(new EmptyBorder(-30,20,-100,-100));
+            areaPrincipal.setFocusable(false);
+            scroll.setBorder(BorderFactory.createEmptyBorder());
+
+            add(concordarJCB = new JCheckBox("Concordo com os termos de uso!"));
+            concordarJCB.setBorder(new EmptyBorder(0,20,0,0));
+            concordarJCB.addActionListener(this);
+        }
+
+        public void actionPerformed(ActionEvent evt)
+        {
+            if(evt.getSource() == concordarJCB)
+            {
+                if(concordarJCB.isSelected())
+                    painelSul.activarBotao(true);
+                else
+                    painelSul.activarBotao(false);
+            }
+        }
+    }
+
+    class PainelSul extends JPanel implements ActionListener
     {
         private JButton entrarJB, sairJB;
 
@@ -61,40 +138,6 @@ public class ApresentacaoVisao extends JFrame
         public void activarBotao(boolean status)
         {
             entrarJB.setEnabled(status);
-        }
-    }
-
-    class PainelCentro extends JPanel implements ActionListener
-    {
-        JTextArea areaPrincipal;
-        JCheckBox concordarJCB;
-
-        public PainelCentro()
-        {
-            setLayout(new GridLayout(2,1));
-            add(new JScrollPane(areaPrincipal = new JTextArea(80,60)));
-            areaPrincipal.setFocusable(false);
-            //Colocar imagem!!!!
-            areaPrincipal.setText("Bem vindo ao Sistema de Gestao de Padaria (SGP) \n\n" + "Este projeto tem o objetivo gerenciar as entradas\n" + "saidas, processo de producao e estoque de uma padaria\n\n" + 
-            "Este projecto foi criado no ambito da cadeira de Fundamentos de Programação II\n" + 
-            "No Curso de Engenharia Informática da UCAN. \n" + 
-            "Se concorda com os termos e condições\n\n" +
-            "Dados do Programador:\n" + "Nome: Valentim Loth Simão Prado\n" + "Nº Processo: 1000033031\n" + 
-            "");
-            add(concordarJCB = new JCheckBox("concordo com os termos de uso!"));
-
-            concordarJCB.addActionListener(this);
-        }
-
-        public void actionPerformed(ActionEvent evt)
-        {
-            if(evt.getSource() == concordarJCB)
-            {
-                if(concordarJCB.isSelected())
-                    painelSul.activarBotao(true);
-                else
-                    painelSul.activarBotao(false);
-            }
         }
     }
 
